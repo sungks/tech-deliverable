7956da4baa61bdb3bc283e6427a16e73c7b5c537
Kaelyn Sung <sungks@uci.edu>
2025-11-18 10:35:30 -0800

    added API route with query parameter to retrieve quotes based on max age in app.py

--- api/src/app.py
+++ api/src/app.py
@@ -1,8 +1,8 @@
 from contextlib import asynccontextmanager
-from datetime import datetime
-from typing import AsyncIterator
+from datetime import datetime, timedelta
+from typing import AsyncIterator, Optional
 
-from fastapi import FastAPI, Form, status
+from fastapi import FastAPI, Form, Query, status
 from fastapi.responses import RedirectResponse
 from typing_extensions import TypedDict
 
@@ -44,7 +44,41 @@ def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
     database["quotes"].append(quote)
 
     # You may modify the return value as needed to support other functionality
-    return RedirectResponse("/", status.HTTP_303_SEE_OTHER)
+    return quote
 
 
 # TODO: add another API route with a query parameter to retrieve quotes based on max age
+@app.get("/quote")
+def get_quote(
+    max_age_days: Optional[int] = Query(
+        None,
+        ge = 0,
+        description = "Max age of quotes in days. 0 or omitted = all time.",
+    )
+)-> list[Quote]:
+    # Retrive the quotes from the database; if max_age_days is None or 0, return all the quotes
+    # otherwise, return only the quotes that are within the last max_age_days days
+
+    quotes = database["quotes"]
+
+    if max_age_days is None or max_age_days == 0:
+        return sorted(quotes, key=lambda q: q["time"]) #sort quotes in chronolical order
+    
+    now = datetime.now
+    cutoff = now - timedelta(days = max_age_days)
+
+    filtered: list[Quote] = []
+    for q in quotes:
+        try:
+            q_time = datetime.fromisoformate(q["time"])
+        except ValueError:
+            #if the time is malformed in any way, choose to keep or skip it
+            filtered.append(q)
+            continue
+        
+        if q_time >= cutoff:
+            filtered.append(q)
+    
+    #sort the final filtered list and return
+    filtered.sort(key = lambda q: q["time"])
+    return filtered
